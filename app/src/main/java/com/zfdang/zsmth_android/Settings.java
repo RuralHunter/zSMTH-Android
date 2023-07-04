415942f76e7617f051360e80afcb3824b7904028
RuralHunter <ruralhunter@gmail.com>
2023-07-04 17:01:52 +0800

    default to not load image from cdn

--- app/src/main/java/com/zfdang/zsmth_android/Settings.java
+++ app/src/main/java/com/zfdang/zsmth_android/Settings.java
@@ -484,7 +484,7 @@ public class Settings {
 
     bLoadOriginalImage = mPreference.getBoolean(LOAD_ORIGINAL_IMAGE, true);
     bLoginWithVerification = mPreference.getBoolean(LOGIN_WITH_VERIFICATION, true);
-    bImageSourceCDN = mPreference.getBoolean(IMAGE_SOURCE_CDN, true);
+    bImageSourceCDN = mPreference.getBoolean(IMAGE_SOURCE_CDN, false);
 
     bBoardMasterOnly = mPreference.getBoolean(BOARD_MASTER_ONLY, false);
 
